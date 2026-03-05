ac82abffabcae6e628a706d50636b7ad1b86f9fc
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-05 17:33:37 +0600

    second day with controller

--- Server/controller/user.controller.js
+++ Server/controller/user.controller.js
@@ -1,3 +1,22 @@
 const TLSSingAPIV2 = require("tls-sig-api-v2");
 const axios = require("axios");
 const User = require("../models/User");
+
+
+
+//helper function to generate userSing
+const generateUserSig = (userId)=>{
+    const SDKAppID = parseInt(process.env.TRTC_APP_ID);
+    const SDKSecreetKey = process.env.TRTC_SECREET;
+
+    if(!SDKAppID || !SDKSecreetKey){
+        throw new Error("Someting went wrong to getting SDKAppId or Key");
+    }
+
+    const api = new TLSSingAPIV2.Api(SDKAppID,SDKSecreetKey);
+    return api.genSig(userId,15552000);
+}
+
+
+
+// function to create user in Tencent server....

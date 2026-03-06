0c9d382c518749243f4a1b02b661527614194078
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-06 19:47:51 +0600

    added userRoutes, forntendLoing3

--- Frontend/src/utils/PrivateRoute.jsx
+++ Frontend/src/utils/PrivateRoute.jsx
@@ -4,4 +4,4 @@ import { Navigate } from 'react-router-dom';
 export default function PrivateRoute({children}) {
     const userId = localStorage.getItem("trtc_userID");
     return userId ? children : <Navigate to="/login"/>
-}
+} 

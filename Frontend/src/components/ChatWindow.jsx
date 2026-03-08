d5191efa6e683b2e35963c825f620a4a84073f24
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-08 19:32:59 +0600

    imported UIkit from tencent

--- Frontend/src/components/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { loginAndGenerateUserSig } from '../services/user.service';
+import {UIKitProvider,useLoginState,loginState,ConversationList, Chat,MessageList,ChatHeader,Avatar,ContactInfo,ContactList,MessageInput,useConversationListState,Profile} from '@tencentcloud/chat-uikit-react';
+
+
 
 export default function ChatWindow() {
+  const [activetab, setActiveTab] = useState('chats');
+  const [userSig, setUserSig] = useState('');
+  const [loading,setLoading] = useState(true);
+  const [mobileView,setMobileView] = useState('list');
+  const [showProfile, setShowProfile] = useState(false);
+  
+  const SDKAppId = import.meta.env.VITE_REACT_TRTC_APP_ID;
+  const userId = localStorage.getItem('trtc_userID');
+
+  useEffect(()=>{
+    const fetchSig = async()=>{
+      try{
+        const res = await loginAndGenerateUserSig(userId);
+        if(res.success){
+          setUserSig(res.user.userSig);
+        }
+      }catch(err){
+        console.error('Signature error',err);
+      }finally{
+        setLoading(false);
+      }
+    };
+
+    if(userId){
+      fetchSig();
+    }
+  },[userId]);
+ 
+
+
+  const handleLogout = () =>{
+    localStorage.removeItem('trtc_userID');
+    window.location.href = '/login'
+
+
+  }
+
+  if(loading){
+
+  }
+
   return (
-    <div>ChatWindow</div>
+
+    <UIKitProvider>
+
+      <div className='flex flex-col md:flex-row h-screen overflow-hidden w-full relative'>
+        <nav className='w-20 bg-gray-900 hidden md:flex flex-col items-center text-white py-6 justify-between z-30 '>
+          <div className="w-full flex flex-col items-center gap-6">
+            <div className="hover:cursor-pointer hover:ring-2 w-10 h-10 ring-indigo-500 transition-all rounded-full"
+              onClick={()=>setShowProfile(true)}
+            >
+              <Avatar size={40}/>
+            </div>
+
+            <div className="w-10 h-[1px] bg-gray-700"></div>
+
+          </div>
+
+
+        </nav>
+      </div>
+
+    </UIKitProvider>
+
   )
 }
--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import ChatWindow from './ChatWindow';
+import {useNavigate} from 'react-router-dom';
+
 
 export default function Dashboard() {
+
+  const navigate = useNavigate();
+  const userId = localStorage.getItem('trtc_userID');
+  if(!userId){
+    navigate('/login');
+    return;
+  }
+
+  
   return (
-    <div>Dashboard</div>
+    <div className='h-screen w-full overflow-hidden bg-white'>
+      <main className='h-full w-full'>
+        <ChatWindow/>
+      </main>
+    </div>
   )
 }
